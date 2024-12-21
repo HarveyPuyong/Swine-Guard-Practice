2fd168df23fc74d70506b624ed605c7e78463ca6
harvey puyong <harveyluslos80@gmail.com>
2024-12-22 00:42:41 +0800

    add marinduque addresses data in select element

---
+++ user/script/handle-address-data.js
@@ -0,0 +1,19 @@
+import { addresses } from "./address-data.js";
+
+export function handleAddressData(){
+  const municipalities = Object.keys(addresses);
+
+  const municipalityOption = municipalities.map(municipal =>  `<option value="${municipal}">${municipal}</option>`);
+
+  const municipalitySelectElement = document.querySelector('.user-adress-form #municipal').innerHTML = municipalityOption
+
+
+  const municipalitySelectElem = document.querySelector('.user-adress-form #municipal');
+
+  municipalitySelectElem.addEventListener('change', ()=>{
+    const barangaysOfMunicipality = addresses[municipalitySelectElem.value];
+    const barangaySelectElement = document.querySelector('.user-adress-form #barangay').innerHTML = 
+    barangaysOfMunicipality.map(barangay => `<option value=${barangay}>${barangay}</option>`);
+  }
+  );
+}
--- user/script/main.js
+++ user/script/main.js
@@ -1,13 +1,17 @@
 import { logInFunctionality } from "./log-in.js";
 import { signUpFunctionality } from "./sign-up.js";
+import { handleAddressData } from "./handle-address-data.js";
 
 logInFunctionality();
 signUpFunctionality();
+handleAddressData();
 
 const formsButton = document.querySelectorAll('form button');
-
-formsButton.forEach(button => {
-  button.addEventListener('click', (e)=> {
-    e.preventDefault();
-  })
+  formsButton.forEach(button => {
+    button.addEventListener('click', (e)=> {
+      if (!form.reportValidity()) {
+        return;
+      }
+      location.reload() = false;
+    })
 })
